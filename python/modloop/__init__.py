a5bd138595a4d9992ea909c00ab2681199f91711
Ben Webb <ben@salilab.org>
2015-05-20 07:47:09 -0700

    Update to latest Modeller.

--- python/modloop/__init__.py
+++ python/modloop/__init__.py
@@ -152,7 +152,7 @@ mkdir -p $tmpdir && cd $tmpdir || exit 1
 # Get input files
 cp %(directory)s/$input %(directory)s/input.pdb .
 
-module load modeller/9.14
+module load modeller/9.15
 python $input ${SGE_TASK_ID} >& $output
 
 # Copy back PDB
