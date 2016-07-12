b7f3f038fec9e843530d298ba3153f2f813f3df9
Ben Webb <ben@salilab.org>
2016-07-12 09:36:55 -0700

    Use new Modeller 9.17 release.

--- python/modloop/__init__.py
+++ python/modloop/__init__.py
@@ -152,7 +152,7 @@ mkdir -p $tmpdir && cd $tmpdir || exit 1
 # Get input files
 cp %(directory)s/$input %(directory)s/input.pdb .
 
-module load modeller/9.16
+module load modeller/9.17
 python $input ${SGE_TASK_ID} >& $output
 
 # Copy back PDB
