47fac8cb8ba87949f0096e9f271a990e4f16458a
Ben Webb <ben@salilab.org>
2019-07-03 13:10:36 -0700

    Start adding a Python Flask frontend

---
+++ frontend/modloop/__init__.py
@@ -0,0 +1,46 @@
+from flask import render_template, request, send_from_directory
+import saliweb.frontend
+from saliweb.frontend import get_completed_job
+
+
+app = saliweb.frontend.make_application(__name__, "##CONFIG##")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
+
+@app.route('/download')
+def download():
+    return render_template('download.html')
+
+
+@app.route('/job', methods=['GET', 'POST'])
+def job():
+    if request.method == 'GET':
+        return saliweb.frontend.render_queue_page()
+    else:
+        return render_template('submit.html')
+
+
+@app.route('/job/<name>')
+def results(name):
+    job = get_completed_job(name, request.args.get('passwd'))
+    return render_template('results.html', job=job)
+
+
+@app.route('/job/<name>/<path:fp>')
+def results_file(name, fp):
+    job = get_completed_job(name, request.args.get('passwd'))
+    return send_from_directory(job.directory, fp)
