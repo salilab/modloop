abf68a5f22fabeb7c3c5d8158788a8e7d0df4a60
Ben Webb <ben@salilab.org>
2021-08-27 12:56:16 -0700

    Dash must be last in regex
    The dash '-' character must be the last character
    in a [] group in a regex, otherwise it is interpreted
    as a range.

--- backend/modloop/__init__.py
+++ backend/modloop/__init__.py
@@ -217,7 +217,7 @@ class Job(saliweb.backend.Job):
     def run(self):
         with open('loops.tsv') as fh:
             loops = fh.read().rstrip('\r\n')
-        if not re.match('[A-Za-z0-9\t _-<>]+$', loops):
+        if not re.match('[A-Za-z0-9\t _<>-]+$', loops):
             raise saliweb.backend.SanityError("Invalid character in loops.tsv")
         loops = loops.split('\t')
         if len(loops) % 4 != 0:
