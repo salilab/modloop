b02d84d51227e42317ff8a69529819db9ecf67af
Ben Webb <ben@salilab.org>
2022-10-28 17:08:29 -0700

    Update to Modeller 10.4

--- backend/modloop/__init__.py
+++ backend/modloop/__init__.py
@@ -188,7 +188,7 @@ mkdir -p $tmpdir && cd $tmpdir || exit 1
 cp %(directory)s/$input %(directory)s/%(input_pdb)s .
 
 module load Sali
-module load modeller/10.3
+module load modeller/10.4
 python $input ${SGE_TASK_ID} >& $output
 
 # Copy back PDB/mmCIF
